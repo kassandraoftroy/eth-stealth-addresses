df80a04f1f1d89073f8a46b8e5ffc51d341d655a
kassandra <0xdada@protonmail.com>
2023-09-06 10:13:16 +0200

    feat: better types

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-use k256;
-use rand;
-use sha3::{Digest, Keccak256};
-use k256::elliptic_curve::group::GroupEncoding;
-use hex;
-use secp256k1::PublicKey;
 use generic_array::GenericArray;
+use hex;
+use k256;
+use k256::elliptic_curve::group::GroupEncoding;
+use rand;
+use secp256k1::PublicKey;
+use sha3::{Digest, Keccak256};
 
+type Address = [u8; 20];
+type StealthMetaAddress = [u8; 66];
+type PublicKeyUncompressed = [u8; 65];
+type PublicKeyCompressed = [u8; 33];
+type PrivateKey = [u8; 32];
 
 fn main() {
     // setup
@@ -16,17 +21,19 @@ fn main() {
     let viewing_pubkey_point = get_pubkey_from_priv(viewing_key_scalar);
 
     // test encode
-    let stealth_meta_address = encode_stealth_meta_address(spending_pubkey_point, viewing_pubkey_point);
-    let spending_key = encode_scalar(spending_key_scalar);
-    let viewing_key = encode_scalar(viewing_key_scalar);
+    let stealth_meta_address =
+        encode_stealth_meta_address(spending_pubkey_point, viewing_pubkey_point);
+    let spending_key = encode_priv(spending_key_scalar);
+    let viewing_key = encode_priv(viewing_key_scalar);
     let spending_pubkey = encode_pubkey(spending_pubkey_point);
     let viewing_pubkey = encode_pubkey(viewing_pubkey_point);
-    
+
     // test decode
-    let (spending_pk_point_check, viewng_pk_point_check) = decode_stealth_meta_address(&stealth_meta_address);
-    let spending_sk_scalar_check = decode_scalar(&spending_key);
+    let (spending_pk_point_check, viewng_pk_point_check) =
+        decode_stealth_meta_address(&stealth_meta_address);
+    let spending_sk_scalar_check = decode_priv(&spending_key);
     let spending_pk_point_check_2 = decode_pubkey(&spending_pubkey);
-    let viewing_sk_scalar_check = decode_scalar(&viewing_key);
+    let viewing_sk_scalar_check = decode_priv(&viewing_key);
     let viewng_pk_point_check_2 = decode_pubkey(&viewing_pubkey);
     assert_eq!(spending_pk_point_check, spending_pubkey_point);
     assert_eq!(viewng_pk_point_check, viewing_pubkey_point);
@@ -36,7 +43,8 @@ fn main() {
     assert_eq!(viewng_pk_point_check_2, viewing_pubkey_point);
 
     // test
-    let (stealth_address, ephemeral_pubkey, view_tag) = generate_stealth_address(&stealth_meta_address);
+    let (stealth_address, ephemeral_pubkey, view_tag) =
+        generate_stealth_address(&stealth_meta_address);
 
     // print
     println!("--- EXAMPLE ---");
@@ -45,11 +53,38 @@ fn main() {
     println!("view tag: {}", view_tag);
 
     // check
-    let check1 = check_stealth_address(&stealth_address, &ephemeral_pubkey, &viewing_key, &spending_pubkey);
-    let check2 = check_stealth_address_fast(&stealth_address, &ephemeral_pubkey, &viewing_key, &spending_pubkey, view_tag);
-    let check3 = check_stealth_address_fast(&stealth_address, &ephemeral_pubkey, &viewing_key, &spending_pubkey, view_tag+1);
-    let check4 = check_stealth_address(&stealth_address, &ephemeral_pubkey, &spending_key, &spending_pubkey);
-    let stealth_key = compute_stealth_key(&stealth_address, &ephemeral_pubkey, &viewing_key, &spending_key);
+    let check1 = check_stealth_address(
+        &stealth_address,
+        &ephemeral_pubkey,
+        &viewing_key,
+        &spending_pubkey,
+    );
+    let check2 = check_stealth_address_fast(
+        &stealth_address,
+        &ephemeral_pubkey,
+        &viewing_key,
+        &spending_pubkey,
+        view_tag,
+    );
+    let check3 = check_stealth_address_fast(
+        &stealth_address,
+        &ephemeral_pubkey,
+        &viewing_key,
+        &spending_pubkey,
+        view_tag + 1, // wrong
+    );
+    let check4 = check_stealth_address(
+        &stealth_address,
+        &ephemeral_pubkey,
+        &spending_key, // wrong
+        &spending_pubkey,
+    );
+    let stealth_key = compute_stealth_key(
+        &stealth_address,
+        &ephemeral_pubkey,
+        &viewing_key,
+        &spending_key,
+    );
     assert_eq!(check1, true);
     assert_eq!(check2, true);
     assert_eq!(check3, false);
@@ -58,9 +93,9 @@ fn main() {
     println!("stealth key: {}", hexlify(&stealth_key));
 }
 
-fn generate_stealth_address(    
-    stealth_meta_address: &[u8; 66],
-) -> ([u8; 20], [u8; 33], u8) {
+fn generate_stealth_address(
+    stealth_meta_address: &StealthMetaAddress,
+) -> (Address, PublicKeyCompressed, u8) {
     let r = k256::Scalar::generate_biased(&mut rand::thread_rng());
     let (spend_pk_point, view_pk_point) = decode_stealth_meta_address(stealth_meta_address);
     let (stealth_pk_point, view_tag) = get_stealth_pubkey(spend_pk_point, view_pk_point, r);
@@ -71,65 +106,75 @@ fn generate_stealth_address(
 }
 
 fn check_stealth_address(
-    stealth_address: &[u8; 20],
-    ephemeral_pubkey: &[u8; 33],
-    viewing_key: &[u8; 32],
-    spending_pubkey: &[u8; 33]
+    stealth_address: &Address,
+    ephemeral_pubkey: &PublicKeyCompressed,
+    viewing_key: &PrivateKey,
+    spending_pubkey: &PublicKeyCompressed,
 ) -> bool {
-    let (shared_secret, _) = get_shared_secret(decode_pubkey(&ephemeral_pubkey), decode_scalar(&viewing_key));
-    let check = get_address_from_pubkey(decode_pubkey(&spending_pubkey)+get_pubkey_from_priv(shared_secret));
+    let (shared_secret, _) = get_shared_secret(
+        decode_pubkey(&ephemeral_pubkey),
+        decode_priv(&viewing_key),
+    );
+    let check = get_address_from_pubkey(
+        decode_pubkey(&spending_pubkey) + get_pubkey_from_priv(shared_secret),
+    );
 
-    return check==*stealth_address;
+    return check == *stealth_address;
 }
 
 fn check_stealth_address_fast(
-    stealth_address: &[u8; 20],
-    ephemeral_pubkey: &[u8; 33],
-    viewing_key: &[u8; 32],
-    spending_pubkey: &[u8; 33],
-    view_tag: u8
+    stealth_address: &Address,
+    ephemeral_pubkey: &PublicKeyCompressed,
+    viewing_key: &PrivateKey,
+    spending_pubkey: &PublicKeyCompressed,
+    view_tag: u8,
 ) -> bool {
-    let (shared_secret, view_tag_check) = get_shared_secret(decode_pubkey(&ephemeral_pubkey), decode_scalar(&viewing_key));
+    let (shared_secret, view_tag_check) = get_shared_secret(
+        decode_pubkey(&ephemeral_pubkey),
+        decode_priv(&viewing_key),
+    );
     if view_tag_check == view_tag {
-        let check = get_address_from_pubkey(decode_pubkey(&spending_pubkey)+get_pubkey_from_priv(shared_secret));
+        let check = get_address_from_pubkey(
+            decode_pubkey(&spending_pubkey) + get_pubkey_from_priv(shared_secret),
+        );
 
-        return check==*stealth_address;
+        return check == *stealth_address;
     } else {
         return false;
     }
 }
 
 fn compute_stealth_key(
-    stealth_address: &[u8; 20],
-    ephemeral_pubkey: &[u8; 33],
-    viewing_key: &[u8; 32],
-    spending_key: &[u8; 32],
-) -> [u8; 32] {
-    let (shared_secret, _) = get_shared_secret(decode_pubkey(&ephemeral_pubkey), decode_scalar(&viewing_key));
-    let stealth_key_scalar = shared_secret+decode_scalar(&spending_key);
+    stealth_address: &Address,
+    ephemeral_pubkey: &PublicKeyCompressed,
+    viewing_key: &PrivateKey,
+    spending_key: &PrivateKey,
+) -> PrivateKey {
+    let (shared_secret, _) = get_shared_secret(
+        decode_pubkey(&ephemeral_pubkey),
+        decode_priv(&viewing_key),
+    );
+    let stealth_key_scalar = shared_secret + decode_priv(&spending_key);
     let stealth_address_check = get_address_from_pubkey(get_pubkey_from_priv(stealth_key_scalar));
     if stealth_address_check != *stealth_address {
         panic!("stealth key does not match stealth address (spend/view keys are not recipient)");
     }
-    return encode_scalar(stealth_key_scalar);
+    return encode_priv(stealth_key_scalar);
 }
 
 fn get_stealth_pubkey(
     pks: k256::ProjectivePoint,
     pkv: k256::ProjectivePoint,
-    r: k256::Scalar
+    r: k256::Scalar,
 ) -> (k256::ProjectivePoint, u8) {
     let (shared_secret, view_tag) = get_shared_secret(pkv, r);
-    let stealth_pub = pks+get_pubkey_from_priv(shared_secret);
+    let stealth_pub = pks + get_pubkey_from_priv(shared_secret);
 
     (stealth_pub, view_tag)
 }
 
-fn get_shared_secret(
-    pubkey: k256::ProjectivePoint,
-    k: k256::Scalar
-) -> (k256::Scalar, u8) {
-    let shared_secret_raw = pubkey*k;
+fn get_shared_secret(pubkey: k256::ProjectivePoint, k: k256::Scalar) -> (k256::Scalar, u8) {
+    let shared_secret_raw = pubkey * k;
     let mut hasher = Keccak256::new();
     hasher.update(shared_secret_raw.to_bytes());
     let hashed_s = hasher.finalize();
@@ -138,45 +183,50 @@ fn get_shared_secret(
         Ok(val) => val,
         Err(error) => panic!("problem generating scalar: {:?}", error),
     };
-    
+
     return (k256::Scalar::from(primitive), hashed_s[0]);
 }
 
-fn encode_stealth_meta_address(pks: k256::ProjectivePoint, pkv: k256::ProjectivePoint) -> [u8; 66] {
+fn encode_stealth_meta_address(
+    pks: k256::ProjectivePoint,
+    pkv: k256::ProjectivePoint,
+) -> StealthMetaAddress {
     let b = [pks.to_bytes(), pkv.to_bytes()].concat();
-    let arr: [u8; 66] = b.as_slice().try_into().unwrap();
+    let arr: StealthMetaAddress = b.as_slice().try_into().unwrap();
 
     arr
 }
 
-fn decode_stealth_meta_address(encoded: &[u8; 66]) -> (k256::ProjectivePoint, k256::ProjectivePoint) {
+fn decode_stealth_meta_address(
+    encoded: &StealthMetaAddress,
+) -> (k256::ProjectivePoint, k256::ProjectivePoint) {
     let mut front = encoded.to_vec();
     let back = front.split_off(33);
-    let front_arr: [u8; 33] = front.as_slice().try_into().unwrap();
-    let back_arr: [u8; 33] = back.as_slice().try_into().unwrap();
+    let front_arr: PublicKeyCompressed = front.as_slice().try_into().unwrap();
+    let back_arr: PublicKeyCompressed = back.as_slice().try_into().unwrap();
 
     return (decode_pubkey(&front_arr), decode_pubkey(&back_arr));
 }
 
-fn encode_pubkey(x: k256::ProjectivePoint) -> [u8; 33] {
+fn encode_pubkey(x: k256::ProjectivePoint) -> PublicKeyCompressed {
     let b = x.to_bytes();
-    let arr: [u8; 33] = b.as_slice().try_into().unwrap();
+    let arr: PublicKeyCompressed = b.as_slice().try_into().unwrap();
 
     arr
 }
 
-fn decode_pubkey(encoded: &[u8; 33]) -> k256::ProjectivePoint {
+fn decode_pubkey(encoded: &PublicKeyCompressed) -> k256::ProjectivePoint {
     return k256::ProjectivePoint::from_bytes(GenericArray::from_slice(encoded)).unwrap();
 }
 
-fn encode_scalar(x: k256::Scalar) -> [u8; 32] {
+fn encode_priv(x: k256::Scalar) -> PrivateKey {
     let b = x.to_bytes();
-    let arr: [u8; 32] = b.as_slice().try_into().unwrap();
+    let arr: PrivateKey = b.as_slice().try_into().unwrap();
 
     arr
 }
 
-fn decode_scalar(encoded: &[u8; 32]) -> k256::Scalar {
+fn decode_priv(encoded: &PrivateKey) -> k256::Scalar {
     let result = k256::elliptic_curve::scalar::ScalarPrimitive::from_slice(encoded);
     let primitive = match result {
         Ok(val) => val,
@@ -186,10 +236,10 @@ fn decode_scalar(encoded: &[u8; 32]) -> k256::Scalar {
 }
 
 fn get_pubkey_from_priv(x: k256::Scalar) -> k256::ProjectivePoint {
-    return k256::ProjectivePoint::GENERATOR*x;
+    return k256::ProjectivePoint::GENERATOR * x;
 }
 
-fn get_address_from_pubkey(pubkey: k256::ProjectivePoint) -> [u8; 20] {
+fn get_address_from_pubkey(pubkey: k256::ProjectivePoint) -> Address {
     let result = PublicKey::from_slice(&pubkey.to_bytes());
     let pub_reformat = match result {
         Ok(val) => val,
@@ -200,17 +250,13 @@ fn get_address_from_pubkey(pubkey: k256::ProjectivePoint) -> [u8; 20] {
     let mut hasher = Keccak256::new();
     hasher.update(pub_hashable);
     let address_raw = hasher.finalize();
-    let mut address_vec_long = address_raw.to_vec();
-    let address_vec = address_vec_long.split_off(12);
-    let address: [u8; 20] = address_vec.as_slice().try_into().unwrap();
+    let address: Address = address_raw[12..].try_into().unwrap();
 
     address
 }
 
-fn strip_uncompressed_pubkey(a: &[u8; 65]) -> [u8; 64] {
-    let mut front = a.to_vec();
-    let back = front.split_off(1);
-    let arr: [u8; 64] = back.as_slice().try_into().unwrap();
+fn strip_uncompressed_pubkey(a: &PublicKeyUncompressed) -> [u8; 64] {
+    let arr: [u8; 64] = a[1..].try_into().unwrap();
 
     arr
 }
